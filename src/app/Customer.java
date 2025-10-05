bb3af9e404164315f2aa00638e6d53dccf4c2f16
Літвінов Данило <l1kdan07@gmail.com>
2025-10-05 18:20:20 +0300

    Add: getters for Customer class

--- src/app/Customer.java
+++ src/app/Customer.java
@@ -11,4 +11,12 @@ public class Customer {
         this.name = name;
         this.phone = phone;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
